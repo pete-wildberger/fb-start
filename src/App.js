f965dadde44c80a273c5d0fcd803dcfb22ab9e31
Peter Wildberger <pete.wildberger@gmail.com>
2018-02-13 18:32:35 -0600

    write to db

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React, { Component } from "react";
-import { database } from "./firebase";
-import "./App.css";
+import React, { Component } from 'react';
+import { database } from './firebase';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null
+      data: null,
+      newData: ''
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   componentDidMount() {
-    database.ref().on("value", snapshot => {
+    database.ref().on('value', snapshot => {
       this.setState({ data: snapshot.val() });
     });
+  }
+  handleChange(event) {
+    const newData = event.target.value;
+    this.setState({ newData });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    const newData = database
+      .ref()
+      .child('AMAZINGDATA')
+      .set(this.state.newData);
   }
   render() {
     return (
@@ -21,6 +35,10 @@ class App extends Component {
           <h1 className="App-title">Welcome to React and firebase</h1>
         </header>
         <pre>{JSON.stringify(this.state.data, null, 2)}</pre>
+        <form onChange={this.handleSubmit}>
+          <input type="text" value={this.state.newData} onChange={this.handleChange} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
